246e7b01861080cd6a765ce7bad9f8d36016d3ee
V.D <87772275+vanshid@users.noreply.github.com>
2023-03-06 16:41:40 +0530

    Bug Fixes

--- auth/settings.py
+++ auth/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%1hl^rjd=bh!5gxi7my54h^lk7(dk-*y8%4qzgnihf-b+y#u0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
